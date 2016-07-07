192601c1b0707c8a30de8dbe62fab9a4664157ea
omegga <substitute.user.do@gmail.com>
2016-07-07 21:05:25 +0000

    add present day as default in url

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 var http = require("http");
 
+var months = [
+  "",
+  "January",
+  "February",
+  "March",
+  "April",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+  ];
+
 function convertDate(path) {
   var timestamp;
   
@@ -12,26 +27,39 @@ function convertDate(path) {
   if (!isNaN(timestamp.getTime())) {
     return {
       unix: timestamp.getTime(),
-      natural:timestamp.toDateString()
+      natural:timestamp.toDateString(),
+      "message to user": "Write a date in the URL to get a timestamp"
     }
   } else {
     return {
       unix: null,
-      natural:null
+      natural:null,
+      "message to user": "Write a date in the URL to get a timestamp"
     }
   }
 }
 
 var server = http.createServer(function(req, res){
   
-  var path = req.url.substr(1);
-  path = decodeURI(path);
+  if (req.url === "/") {
+    var date = new Date();
+    var d = date.getDay();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+    var urlToAdd = "/"+months[m] + " " + d + ", " + y;
+    res.writeHead(301, {Location:urlToAdd});
+    res.end();
+  } else {
+    var path = req.url.substr(1);
+    path = decodeURI(path);
   
-  var result = convertDate(path)
+    var result = convertDate(path)
   
   
-  res.writeHead(200, {"Content-Type":"text/json"});
-  res.end(JSON.stringify(result))
+    res.writeHead(200, {"Content-Type":"text/json"});
+    res.end(JSON.stringify(result))
+  }
+  
   
 });
 
